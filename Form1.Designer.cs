c23d4059a99c71e4510715b2555888a14b1a5fb6
Josh Peterson <petersonjm1@gmail.com>
2009-08-21 06:20:18 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PhotoSorter
+{
+    partial class PhotoSorter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoSorter));
+            this.PhotoDisplay = new System.Windows.Forms.ListView();
+            this.SourceDirectory = new System.Windows.Forms.Button();
+            this.AddDirectory = new System.Windows.Forms.Button();
+            this.DestinationDirectoriesPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.CopyPhotos = new System.Windows.Forms.Button();
+            this.DeleteCheckbox = new System.Windows.Forms.CheckBox();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.RemoveAllDestinations = new System.Windows.Forms.Button();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PhotoDisplay
+            // 
+            this.PhotoDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.PhotoDisplay.Location = new System.Drawing.Point(12, 12);
+            this.PhotoDisplay.Name = "PhotoDisplay";
+            this.PhotoDisplay.Size = new System.Drawing.Size(530, 454);
+            this.PhotoDisplay.TabIndex = 0;
+            this.PhotoDisplay.UseCompatibleStateImageBehavior = false;
+            // 
+            // SourceDirectory
+            // 
+            this.SourceDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SourceDirectory.Location = new System.Drawing.Point(548, 12);
+            this.SourceDirectory.Name = "SourceDirectory";
+            this.SourceDirectory.Size = new System.Drawing.Size(110, 23);
+            this.SourceDirectory.TabIndex = 1;
+            this.SourceDirectory.Text = "Find Photos";
+            this.SourceDirectory.UseVisualStyleBackColor = true;
+            this.SourceDirectory.Click += new System.EventHandler(this.SourceDirectoryOnClick);
+            // 
+            // AddDirectory
+            // 
+            this.AddDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddDirectory.Location = new System.Drawing.Point(548, 41);
+            this.AddDirectory.Name = "AddDirectory";
+            this.AddDirectory.Size = new System.Drawing.Size(110, 23);
+            this.AddDirectory.TabIndex = 3;
+            this.AddDirectory.Text = "Add Destination";
+            this.AddDirectory.UseVisualStyleBackColor = true;
+            this.AddDirectory.Click += new System.EventHandler(this.AddDirectoryOnClick);
+            // 
+            // DestinationDirectoriesPanel
+            // 
+            this.DestinationDirectoriesPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DestinationDirectoriesPanel.AutoScroll = true;
+            this.DestinationDirectoriesPanel.Location = new System.Drawing.Point(551, 151);
+            this.DestinationDirectoriesPanel.Name = "DestinationDirectoriesPanel";
+            this.DestinationDirectoriesPanel.Padding = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.DestinationDirectoriesPanel.Size = new System.Drawing.Size(118, 328);
+            this.DestinationDirectoriesPanel.TabIndex = 4;
+            // 
+            // CopyPhotos
+            // 
+            this.CopyPhotos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CopyPhotos.Location = new System.Drawing.Point(548, 99);
+            this.CopyPhotos.Name = "CopyPhotos";
+            this.CopyPhotos.Size = new System.Drawing.Size(109, 23);
+            this.CopyPhotos.TabIndex = 5;
+            this.CopyPhotos.Text = "Copy Photos";
+            this.CopyPhotos.UseVisualStyleBackColor = true;
+            this.CopyPhotos.Click += new System.EventHandler(this.CopyPhotosOnClick);
+            // 
+            // DeleteCheckbox
+            // 
+            this.DeleteCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteCheckbox.AutoSize = true;
+            this.DeleteCheckbox.Location = new System.Drawing.Point(548, 128);
+            this.DeleteCheckbox.Name = "DeleteCheckbox";
+            this.DeleteCheckbox.Size = new System.Drawing.Size(118, 17);
+            this.DeleteCheckbox.TabIndex = 6;
+            this.DeleteCheckbox.Text = "Remove After Copy";
+            this.DeleteCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusStripLabel});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 469);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(670, 22);
+            this.StatusStrip.TabIndex = 7;
+            // 
+            // StatusStripLabel
+            // 
+            this.StatusStripLabel.Name = "StatusStripLabel";
+            this.StatusStripLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // RemoveAllDestinations
+            // 
+            this.RemoveAllDestinations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RemoveAllDestinations.Location = new System.Drawing.Point(548, 70);
+            this.RemoveAllDestinations.Name = "RemoveAllDestinations";
+            this.RemoveAllDestinations.Size = new System.Drawing.Size(109, 23);
+            this.RemoveAllDestinations.TabIndex = 8;
+            this.RemoveAllDestinations.Text = "Delete Destinations";
+            this.RemoveAllDestinations.UseVisualStyleBackColor = true;
+            this.RemoveAllDestinations.Click += new System.EventHandler(this.RemoveAllDestinationsOnClick);
+            // 
+            // PhotoSorter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 491);
+            this.Controls.Add(this.RemoveAllDestinations);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.DeleteCheckbox);
+            this.Controls.Add(this.CopyPhotos);
+            this.Controls.Add(this.DestinationDirectoriesPanel);
+            this.Controls.Add(this.AddDirectory);
+            this.Controls.Add(this.SourceDirectory);
+            this.Controls.Add(this.PhotoDisplay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PhotoSorter";
+            this.Text = "Cindy\'s Photo Sorter";
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView PhotoDisplay;
+        private System.Windows.Forms.Button SourceDirectory;
+        private System.Windows.Forms.Button AddDirectory;
+        private System.Windows.Forms.FlowLayoutPanel DestinationDirectoriesPanel;
+        private System.Windows.Forms.Button CopyPhotos;
+        private System.Windows.Forms.CheckBox DeleteCheckbox;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel StatusStripLabel;
+        private System.Windows.Forms.Button RemoveAllDestinations;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace PhotoSorter
+{
+    public partial class PhotoSorter : Form
+    {
+        public PhotoSorter()
+        {
+            InitializeComponent();
+            this.destinationDirectories = new List<DestinationDirectoryInformation>();
+            this.StatusStripLabel.Text = "Click Find Photos to select current photo location.";
+        }
+
+        private void SourceDirectoryOnClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            folderBrowser.ShowDialog();
+
+            if (!string.IsNullOrEmpty(folderBrowser.SelectedPath))
+            {
+                this.PhotoDisplay.Clear();
+
+                ImageList images = new ImageList();
+                images.ImageSize = new Size(128, 96);
+                images.ColorDepth = ColorDepth.Depth24Bit;
+                this.PhotoDisplay.LargeImageList = images;
+
+                int i = 0;
+                foreach (string fileName in GetImageFilesInDirectory(folderBrowser.SelectedPath))
+                {
+                    Bitmap image = new Bitmap(fileName);
+                    images.Images.Add(image);
+                    ListViewItem item = new ListViewItem(Path.GetFileName(fileName));
+                    item.ImageIndex = i;
+                    item.Tag = fileName;
+                    this.PhotoDisplay.Items.Add(item);
+                    i++;
+                }    
+            }
+
+            this.StatusStripLabel.Text = "Click Add Destination to select photo destinations.";
+        }
+
+        private void AddDirectoryOnClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            folderBrowser.ShowDialog();
+
+            if (!string.IsNullOrEmpty(folderBrowser.SelectedPath))
+            {
+                DestinationDirectoryInformation destinationDirectory = new DestinationDirectoryInformation(folderBrowser.SelectedPath);
+                this.destinationDirectories.Add(destinationDirectory);
+                destinationDirectory.AddToPanel(this.DestinationDirectoriesPanel);
+            }
+
+            this.StatusStripLabel.Text = "Click Copy Photos to copy photos to each destination.";
+        }
+
+        private void RemoveAllDestinationsOnClick(object sender, EventArgs e)
+        {
+            this.DestinationDirectoriesPanel.Controls.Clear();
+            this.destinationDirectories.Clear();
+
+            this.StatusStripLabel.Text = "Click Add Destination to select photo destinations.";
+        }
+
+        private void CopyPhotosOnClick(object sender, EventArgs e)
+        {
+            int fileNameCounter = 1;
+            int numPhotos = this.PhotoDisplay.SelectedItems.Count;
+            if (numPhotos > 0)
+            {
+                this.StatusStripLabel.Text = "Copying photos...";
+            }
+            else
+            {
+                this.StatusStripLabel.Text = "Select photos to copy.";
+            }
+
+            foreach (ListViewItem photo in this.PhotoDisplay.SelectedItems)
+            {
+                string oldFileName = photo.Tag.ToString();
+                foreach (DestinationDirectoryInformation directory in this.destinationDirectories)
+                {
+                    string newFileName = Path.Combine(directory.DestinationDirectoryName, string.Format("{0} {1}{2}", directory.FileNamePrefix, this.GetFileNameNumber(fileNameCounter, numPhotos), Path.GetExtension(oldFileName)));
+                    while (File.Exists(newFileName))
+                    {
+                        fileNameCounter++;
+                        newFileName = Path.Combine(directory.DestinationDirectoryName, string.Format("{0} {1}{2}", directory.FileNamePrefix, this.GetFileNameNumber(fileNameCounter, numPhotos), Path.GetExtension(oldFileName)));
+                    }
+
+                    File.Copy(oldFileName, newFileName);
+                }
+
+                if (this.DeleteCheckbox.Checked)
+                {
+                    File.Delete(oldFileName);
+                }
+
+                fileNameCounter++;
+            }
+
+            if (numPhotos > 0)
+            {
+                this.StatusStripLabel.Text = "Photos copied successfully.";
+            }
+        }
+
+        private string GetFileNameNumber(int fileNameCounter, int numPhotos)
+        {
+            return fileNameCounter.ToString();
+        }
+
+        private IEnumerable<string> GetImageFilesInDirectory(string directoryName)
+        {
+            List<string> imageFileNames = new List<string>();
+            foreach (string fileName in Directory.GetFiles(directoryName))
+            {
+                if (IsImageFile(fileName))
+                {
+                    imageFileNames.Add(fileName);
+                }
+            }
+            return imageFileNames;
+        }
+
+        private bool IsImageFile(string fileName)
+        {
+            string extension = Path.GetExtension(fileName).ToLower();
+            return extension == ".jpg" || extension == ".png";
+        }
+
+        struct DestinationDirectoryInformation
+        {
+            public DestinationDirectoryInformation(string destinationDirectoryName)
+            {
+                this.destinationDirectoryName = Path.GetFullPath(destinationDirectoryName);
+                this.destinationDirectoryDisplay = new Label();
+                this.destinationDirectoryDisplay.Height = 1;
+                this.destinationDirectoryDisplay.AutoSize = true;
+                this.destinationDirectoryDisplay.Text = this.destinationDirectoryName.Substring(this.destinationDirectoryName.LastIndexOf(Path.DirectorySeparatorChar)+1);
+                
+                ToolTip fullDirectoryName = new ToolTip();
+                fullDirectoryName.SetToolTip(this.destinationDirectoryDisplay, string.Format("Destination directory: {0}", this.destinationDirectoryName));
+
+                this.fileNamePrefix = new TextBox();
+                this.fileNamePrefix.Text = "File name prefix";
+                this.fileNamePrefix.Click += new EventHandler(this.FileNamePrefixOnClick);
+            }
+
+            public void AddToPanel(Panel parent)
+            {
+                FlowLayoutPanel directoryPanel = new FlowLayoutPanel();
+                directoryPanel.Height = 1;
+                directoryPanel.AutoSize = true;
+                directoryPanel.Controls.Add(this.destinationDirectoryDisplay);
+                directoryPanel.Controls.Add(this.fileNamePrefix);
+
+                parent.Controls.Add(directoryPanel);
+            }
+
+            public string FileNamePrefix
+            {
+                get
+                {
+                    return this.fileNamePrefix.Text;
+                }
+            }
+
+            public string DestinationDirectoryName
+            {
+                get
+                {
+                    return this.destinationDirectoryName;
+                }
+            }
+
+            private void FileNamePrefixOnClick(object sender, EventArgs e)
+            {
+                TextBox fileNamePrefix = sender as TextBox;
+                fileNamePrefix.Text = string.Empty;
+            }
+
+            private string destinationDirectoryName;
+            private Label destinationDirectoryDisplay;
+            private TextBox fileNamePrefix;
+        }
+
+        private List<DestinationDirectoryInformation> destinationDirectories;
+    }
+}
