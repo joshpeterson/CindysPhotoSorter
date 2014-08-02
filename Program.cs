3bb270d7a2998b9d3a55e867fcf6e958384e093d
Joshua Peterson <petersonjm1@gmail.com>
2014-08-02 15:22:20 -0400

    Embed the Ookii assembly as a resource.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace CindysPhotoSorter
 {
     static class Program
     {
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains("Ookii"))
+            {
+                string resourceName = string.Format("CindysPhotoSorter.{0}.dll", new AssemblyName(args.Name).Name);
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            }
+
+            return null;
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,5 +40,7 @@ namespace CindysPhotoSorter
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PhotoSorterForm());
         }
+
+
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Prevent calls to the assembly resolve event handler for resoruces.
+[assembly: NeutralResourcesLanguageAttribute("en-US", UltimateResourceFallbackLocation.MainAssembly)]
